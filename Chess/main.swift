851b201f74e1ada733323f58085735d28489f911
Eirik Vale Aase <e.v.aase@gmail.com>
2019-12-26 16:24:33 +0100

    Initial Commit

---
+++ Chess/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Chess
+//
+//  Created by Eirik Vale Aase on 26/12/2019.
+//  Copyright © 2019 Eirik Vale Aase. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
