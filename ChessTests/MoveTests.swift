cb530fe4129dc9024ee5564a3ad727cd2fdd1ece
Eirik Vale Aase <e.v.aase@gmail.com>
2019-12-29 00:25:34 +0100

    Add move tests file

---
+++ ChessTests/MoveTests.swift
@@ -0,0 +1,15 @@
+//
+//  MoveTests.swift
+//  ChessTests
+//
+//  Created by Eirik Vale Aase on 29/12/2019.
+//  Copyright © 2019 Eirik Vale Aase. All rights reserved.
+//
+
+import XCTest
+
+@testable import Chess
+
+class MoveTests: XCTestCase {
+    
+}
