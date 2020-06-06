328849a15319b63e188a0cf6f92d73132dbb41be
Eirik Vale Aase <e.v.aase@gmail.com>
2020-06-06 16:11:22 +0200

    Clean up move validation

--- Chess/Model/Game/Validators.swift
+++ Chess/Model/Game/Validators.swift
@@ -46,10 +46,20 @@ struct MoveValidator {
         
         let possibleSourcePieces = board.getPieces(of: move.pieceType, side: currentSide)
         
-        let filteredSourceCoordinates: [Piece] = possibleSourcePieces.compactMap {
+        let filteredSourcePieces: [Piece] = possibleSourcePieces.compactMap {
             let possibleSource = board.getCoordinate(of: $0)
             let delta = move.destination - possibleSource
             let validPattern = $0.validPattern(delta: delta, side: currentSide, isCapture: move.isCapture())
+            
+            // Cannot move pieces of the other side
+            guard board[possibleSource]?.side == currentSide else {
+                return nil
+            }
+            
+            // Cannot move own pieces to positions occupied by own pieces
+            if board[move.destination]?.side == currentSide {
+                return nil
+            }
             
             // Disambiguation happens first with the file, then the rank, then the file and rank.
             if move.source?.file == nil && move.source?.rank != nil {
@@ -91,50 +101,24 @@ struct MoveValidator {
             return nil
         }
         
-        guard filteredSourceCoordinates.count == 1 else {
-            throw GameError.invalidMove(message: "[\(move.rawInput)] Found zero or multiple possible source pieces: \(filteredSourceCoordinates.map({board.getCoordinate(of: $0)}))")
+        guard let sourcePiece = filteredSourcePieces.first else {
+            throw GameError.invalidMove(message: "[\(move.rawInput)] Found zero source pieces.")
         }
         
-        let sourceCoordinate = board.getCoordinate(of: filteredSourceCoordinates[0])
+        move.source = board.getCoordinate(of: sourcePiece)
         
         if move.isEnPassant() {
             // TODO: Implement validation for en passant
-            // move.options.append(.enPassant)
-            // move.source = sourceCoordinate
-            move.source = sourceCoordinate
             return
         }
         
-        move.source = sourceCoordinate
-        let destinationCoordinate = move.destination
-        let destinationPiece = board[destinationCoordinate]
-        let moveDelta = destinationCoordinate - sourceCoordinate
+        let destinationPiece = board[move.destination]
         
-        guard let sourcePiece = board[sourceCoordinate] else {
-            throw GameError.noPieceInSourcePosition
-        }
-
-        let validPattern = sourcePiece.validPattern(delta: moveDelta, side: currentSide, isCapture: move.isCapture())
-        
-        guard validPattern.directions.count > 0 else {
-            throw GameError.invalidMove(message: "No valid directions to destination position")
-        }
-
-        guard sourcePiece.side == currentSide else {
-            throw GameError.invalidPiece
-        }
-
-        if destinationPiece?.side == currentSide {
-            throw GameError.invalidMove(message: "Cannot move to position occupied by self")
-        }
-        
-        for direction in validPattern.directions {
-            if sourcePiece.type == .pawn {
-                if [.north, .south].contains(direction) && destinationPiece != nil {
-                    throw GameError.invalidMove(message: "[\(move.rawInput)] Cannot move pawn north -- desination is occupied.")
-                } else if [.northEast, .northWest, .southEast, .southWest].contains(direction) && destinationPiece == nil {
-                    throw GameError.invalidMove(message: "[\(move.rawInput)] Cannt move pawn diagonally -- destination is empty.")
-                }
+        if sourcePiece.type == .pawn {
+            if !move.isCapture() && destinationPiece != nil {
+                throw GameError.invalidMove(message: "[\(move.rawInput)] Cannot move pawn north -- desination is occupied.")
+            } else if move.isCapture() && destinationPiece == nil {
+                throw GameError.invalidMove(message: "[\(move.rawInput)] Cannt move pawn diagonally -- destination is empty.")
             }
         }
     }
