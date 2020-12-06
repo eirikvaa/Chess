24c89a1a31d653eadb8c51a6fea64cc378fbba2b
Eirik Vale Aase <e.v.aase@gmail.com>
2020-12-06 16:19:49 +0100

    Extract file and rank classes to their own files

---
+++ Chess/V2/File.swift
@@ -0,0 +1,59 @@
+//
+//  File.swift
+//  Chess
+//
+//  Created by Eirik Vale Aase on 06/12/2020.
+//  Copyright © 2020 Eirik Vale Aase. All rights reserved.
+//
+
+import Foundation
+
+/**
+ A file is a (vertical) column as seen from white's perspective.
+ Goes from "a" to "h" from left to right.
+ */
+struct File: Equatable, CustomStringConvertible, ExpressibleByStringLiteral {
+    let value: String
+    static var validFiles = ["a", "b", "c", "d", "e", "f", "g", "h"]
+    var index: Int {
+        let validFiles = ["a", "b", "c", "d", "e", "f", "g", "h"]
+        return validFiles.firstIndex(of: value)!
+    }
+    
+    init(stringLiteral value: String) {
+        self.value = value
+    }
+    
+    init(value: String) {
+        self.value = value
+    }
+    
+    static func == (lhs: File, rhs: File) -> Bool {
+        lhs.value == rhs.value
+    }
+    
+    static func + (lhs: File, rhs: Direction) -> File {
+        guard let lhsIndex: Int = validFiles.firstIndex(of: lhs.value) else {
+            fatalError("Cannot find index of \(lhs.value), this should be impossible.")
+        }
+        
+        let deltaX: Int
+        
+        switch rhs {
+        case .east: deltaX = 1
+        case .west: deltaX = -1
+        default: deltaX = 0
+        }
+        
+        let newIndex = lhsIndex + deltaX
+        guard newIndex < validFiles.count else {
+            fatalError("Index \(newIndex) is invalid.")
+        }
+        
+        return File(stringLiteral: validFiles[newIndex])
+    }
+    
+    var description: String {
+        value
+    }
+}
---
+++ Chess/V2/Rank.swift
@@ -0,0 +1,50 @@
+//
+//  Rank.swift
+//  Chess
+//
+//  Created by Eirik Vale Aase on 06/12/2020.
+//  Copyright © 2020 Eirik Vale Aase. All rights reserved.
+//
+
+import Foundation
+
+/**
+ A rank is a row as seen from white's perspective.
+ Goes from 1 to 8 from nearest to farthest.
+ */
+struct Rank: Equatable, CustomStringConvertible, ExpressibleByIntegerLiteral {
+    let value: Int
+    
+    init(integerLiteral value: Int) {
+        self.value = value
+    }
+    
+    init(value: Int) {
+        self.value = value
+    }
+    
+    static func == (lhs: Rank, rhs: Rank) -> Bool {
+        lhs.value == rhs.value
+    }
+    
+    static func + (lhs: Rank, rhs: Direction) -> Rank {
+        let deltaY: Int
+        switch rhs {
+        case .north: deltaY = 1
+        case .south: deltaY = -1
+        default: deltaY = 0
+        }
+        
+        let newIndex = lhs.value + deltaY
+        
+        guard 1...8 ~= newIndex else {
+            fatalError("Rank \(newIndex) is impossible to attain.")
+        }
+        
+        return Rank(value: newIndex)
+    }
+    
+    var description: String {
+        "\(value)"
+    }
+}
