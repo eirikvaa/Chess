4149c64aad0fabd5ae42f09202950dab7347110e
Eirik Vale Aase <e.v.aase@gmail.com>
2020-12-24 10:57:28 +0100

    Remove whitespace

--- ChessTests/GameTests.swift
+++ ChessTests/GameTests.swift
@@ -81,7 +81,7 @@ class Game2Tests: XCTestCase {
     func testBlackCanQueenSideCastle() throws {
         assertMoves("e4", "d62", "d4", "Bg4", "c4", "Nc64", "b4", "Qd7", "a4", "O-O-O", throws: .noThrow)
     }
-    
+
     func testValidEnPassant() throws {
         assertMoves("e3", "a6", "e4", "a5", "e5", "f5", "xf6", throws: .noThrow)
     }
