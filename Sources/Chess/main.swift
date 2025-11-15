ce7e1d954b5d83823ed6dff845567a3f300c00ba
Eirik Vale Aase <e.v.aase@gmail.com>
2025-11-15 11:38:33 +0100

    Remove contents of main file

--- Sources/Chess/main.swift
+++ Sources/Chess/main.swift
@@ -5,20 +5,3 @@
 //  Created by Eirik Vale Aase on 26/12/2019.
 //
 
-let game2 = Game()
-
-try game2.play()
-
-let games = try PGNGameReader.readFile("twic920")
-
-for (index, game) in games[27...].enumerated() {
-    print("STARTING GAME \(index + 27)")
-
-    let moves = PGNGameReader.read(textRepresentation: game)
-    do {
-        try Game().applyMoves(moves)
-    } catch let error as GameState.GameStateError {
-        print(error)
-        print(game)
-    }
-}
