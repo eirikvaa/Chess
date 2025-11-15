7236c52ebebd4d1bd0a832bd793d2fa1cf3e0733
Eirik Vale Aase <e.v.aase@gmail.com>
2025-11-15 09:00:20 +0100

    Small improvements to the assertMoves method

--- Tests/ChessTests/GameTests.swift
+++ Tests/ChessTests/GameTests.swift
@@ -13,110 +13,110 @@ import Testing
 struct GameTests {
     @Test
     func pawnMoveToE4() {
-        assertMoves("e4", throws: .noThrow)
+        assertMoves("e4", shouldThrow: .noThrow)
     }
 
     @Test
     func knightToC3() {
-        assertMoves("Nc3", throws: .noThrow)
+        assertMoves("Nc3", shouldThrow: .noThrow)
     }
 
     @Test
     func bishopCannotMoveToPieceOccupiedByItsOwnSide() {
-        assertMoves("B2d", throws: .doThrow)
+        assertMoves("B2d", shouldThrow: .doThrow)
     }
 
     @Test
     func knightDoubleMovesAndCapturesPawn() {
-        assertMoves("Nc3", "d5", "Nxd5", throws: .noThrow)
+        assertMoves("Nc3", "d5", "Nxd5", shouldThrow: .noThrow)
     }
 
     @Test
     func pawnsCannotAttackForward() {
-        assertMoves("e4", "e5", "xe5", throws: .doThrow)
+        assertMoves("e4", "e5", "xe5", shouldThrow: .doThrow)
     }
 
     @Test
     func pawnLegalAttack() {
-        assertMoves("e4", "d5", "xd5", throws: .noThrow)
+        assertMoves("e4", "d5", "xd5", shouldThrow: .noThrow)
     }
 
     @Test
     func whitePawnCannotAttackBackwards() {
-        assertMoves("e4", "d5", "e5", "d4", "xd4", throws: .doThrow)
+        assertMoves("e4", "d5", "e5", "d4", "xd4", shouldThrow: .doThrow)
     }
 
     @Test
     func blackQueenTakesWhiteKnight() {
-        assertMoves("Nc3", "d5", "Nxd5", "Qxd5", throws: .noThrow)
+        assertMoves("Nc3", "d5", "Nxd5", "Qxd5", shouldThrow: .noThrow)
     }
 
     @Test("Rook can move north")
     func rookCanMoveNorth() {
-        assertMoves("a4", "a5", "Ra3", throws: .noThrow)
+        assertMoves("a4", "a5", "Ra3", shouldThrow: .noThrow)
     }
 
     @Test("Should pick correct rook to h3")
     func shouldPickCorrectRookToH3() {
-        assertMoves("a4", "a5", "Ra3", "Ra6", "Rh3", throws: .noThrow)
+        assertMoves("a4", "a5", "Ra3", "Ra6", "Rh3", shouldThrow: .noThrow)
     }
 
     @Test("Rook cannot capture own pieces")
     func rookCannotCaptureOwnPieces() {
-        assertMoves("Rxb1", throws: .doThrow)
+        assertMoves("Rxb1", shouldThrow: .doThrow)
     }
 
     @Test
     func legalKingMove() {
-        assertMoves("e4", "e5", "Ke2", throws: .noThrow)
+        assertMoves("e4", "e5", "Ke2", shouldThrow: .noThrow)
     }
 
     @Test
     func pawnsCannotMoveDiagonallyUnlessItCapturesLegally() {
-        assertMoves("Nf3", "g6", throws: .noThrow)
+        assertMoves("Nf3", "g6", shouldThrow: .noThrow)
     }
 
     @Test
     func canHandleKingSideCastlingCorrectly() {
         assertMoves(
             "Nf3", "g6", "e4", "c5", "c4", "Bg7", "d4", "cxd4", "Nxd4", "Nc6", "Be3",
-            "Nf6", "Nc3", "O-O", throws: .noThrow)
+            "Nf6", "Nc3", "O-O", shouldThrow: .noThrow)
     }
 
     @Test
     func whiteCanKingSideCastle() {
-        assertMoves("e4", "e5", "Nf3", "d5", "Bc4", "c5", "O-O", throws: .noThrow)
+        assertMoves("e4", "e5", "Nf3", "d5", "Bc4", "c5", "O-O", shouldThrow: .noThrow)
     }
 
     @Test
     func whiteCanQueenSideCastle() {
-        assertMoves("e4", "e5", "d4", "d5", "Bf4", "c5", "Nc3", "b5", "O-O-O", throws: .noThrow)
+        assertMoves("e4", "e5", "d4", "d5", "Bf4", "c5", "Nc3", "b5", "O-O-O", shouldThrow: .noThrow)
     }
 
     @Test
     func blackCanKingSideCastle() {
-        assertMoves("e4", "Nf6", "d4", "e6", "c4", "Bd6", "b4", "O-O", throws: .noThrow)
+        assertMoves("e4", "Nf6", "d4", "e6", "c4", "Bd6", "b4", "O-O", shouldThrow: .noThrow)
     }
 
     @Test
     func blackCanQueenSideCastle() {
         assertMoves(
-            "e4", "d62", "d4", "Bg4", "c4", "Nc64", "b4", "Qd7", "a4", "O-O-O", throws: .noThrow)
+            "e4", "d62", "d4", "Bg4", "c4", "Nc64", "b4", "Qd7", "a4", "O-O-O",shouldThrow: .noThrow)
     }
 
     @Test
     func validEnPassantByWhite() {
-        assertMoves("e3", "a6", "e4", "a5", "e5", "f5", "xf6", throws: .noThrow)
+        assertMoves("e3", "a6", "e4", "a5", "e5", "f5", "xf6", shouldThrow: .noThrow)
     }
 
     @Test
     func validEnPassantByBlack() {
-        assertMoves("a3", "e5", "a4", "e4", "d4", "exd3", throws: .noThrow)
+        assertMoves("a3", "e5", "a4", "e4", "d4", "exd3", shouldThrow: .noThrow)
     }
 
     @Test
     func invalidEnPassantPawnMustMoveDoubleAtFirstMove() {
-        assertMoves("e3", "a6", "e4", "a5", "f6", "b3", "f5", "xf6", throws: .doThrow)
+        assertMoves("e3", "a6", "e4", "a5", "f6", "b3", "f5", "xf6", shouldThrow: .doThrow)
     }
 
     @Test
@@ -125,7 +125,7 @@ struct GameTests {
             "Nf3", "g6", "e4", "c5", "c4", "Bg7", "d4", "cxd4", "Nxd4",
             "Nc6", "Be3", "Nf6", "Nc3", "O-O", "Be2", "d6", "O-O", "Nxd4",
             "Bxd4", "Bd7", "Qd2", "Bc6", "f3", "a5", "b3", "Nd7", "Be3", "Nc5",
-            "Rab1", throws: .noThrow)
+            "Rab1", shouldThrow: .noThrow)
     }
 
     @Test
@@ -137,7 +137,7 @@ struct GameTests {
             "Rab1", "Qb6", "Rfc1", "Rfc8", "Rc2", "h5", "Bf1", "Kh7", "g3", "Qd8",
             "Bh3", "e6", "Rd1", "Be5", "Nb5", "Qf8", "Qe2", "Rd8", "Bg5", "Rd7", "Nd4",
             "f5", "Nxc6", "bxc6", "Be3", "Qe7", "Bg2", "Bg7", "Rcd2",
-            "Rad8", "Bxc5", "dxc5", throws: .noThrow)
+            "Rad8", "Bxc5", "dxc5", shouldThrow: .noThrow)
     }
 
     @Test
@@ -145,7 +145,7 @@ struct GameTests {
         assertMoves(
             "e4", "e5", "Nf3", "Nc6", "Bb5", "Nf6", "d3", "Bc5", "c3", "O-O",
             "O-O", "d6", "Nbd2", "a6", "Bxc6", "bxc6", "Re1", "Re8", "h3", "Bb6", "Nf1",
-            throws: .noThrow)
+            shouldThrow: .noThrow)
     }
 
     private enum Throw {
@@ -153,9 +153,13 @@ struct GameTests {
         case noThrow
     }
 
-    private func assertMoves(_ moves: String..., throws: Throw) {
+    private func assertMoves(_ moves: String..., shouldThrow: Throw) {
+        assertMoves(moves, shouldThrow: shouldThrow)
+    }
+
+    private func assertMoves(_ moves: [String], shouldThrow: Throw) {
         let game = Game()
-        switch `throws` {
+        switch shouldThrow {
         case .doThrow:
             #expect(throws: (any Error).self) {
                 try game.applyMoves(moves)
